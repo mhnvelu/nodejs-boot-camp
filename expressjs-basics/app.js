390733a75e44603e7545fd76349efb5869969a28
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-07-08 13:49:26 +0100

    Express server with middlewares

---
+++ expressjs-basics/app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("In Middleware 1");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In Middleware 2");
+  //   res.send("<h2>Response from Middleware 2</h2>");
+  res.send("Response from Middleware 2");
+});
+
+const server = http.createServer(app);
+server.listen(3000);
