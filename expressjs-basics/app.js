32439bc988c4e4bfb2b8d0a9a12388d6069733cc
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-07-08 17:34:35 +0100

    Express Router

--- expressjs-basics/app.js
+++ expressjs-basics/app.js
@@ -1,6 +1,8 @@
 // const http = require("http");
 const express = require("express");
 const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,30 +20,14 @@ app.use((req, res, next) => {
   //   res.send("<h2>Response from Middleware 2</h2>");
 });
 
+app.use(adminRoutes);
+
 app.use("/user", (req, res, next) => {
   console.log("Handled /user endpoint");
   res.send("<h2>Response from user endpoint</h2>");
 });
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="product"><button type="submit">Submit</button> </form>'
-  );
-});
-
-app.use("/product", (req, res, next) => {
-  console.log("Handled /product endpoint");
-  const product = req.body;
-  console.log(product);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("Handled root endpoint");
-  const product = req.body;
-  console.log(product);
-  res.send("<h2>Response from root endpoint</h2>");
-});
+app.use(shopRoutes);
 
 // const server = http.createServer(app);
 // server.listen(3000);
---
+++ expressjs-basics/routes/admin.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="product"><button type="submit">Submit</button> </form>'
+  );
+});
+
+router.post("/product", (req, res, next) => {
+  console.log("Handled /product endpoint");
+  const product = req.body;
+  console.log(product);
+  res.redirect("/");
+});
+
+module.exports = router;
