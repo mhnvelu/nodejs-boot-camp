a715d11f1942af563d9445e16650de9523ea19d7
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-07-08 16:33:32 +0100

    Added middlewares

--- expressjs-basics/app.js
+++ expressjs-basics/app.js
@@ -4,14 +4,26 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
+  console.log("-----------------------");
   console.log("In Middleware 1");
   next();
+  // res.send("Response from Middleware 1");
 });
 
 app.use((req, res, next) => {
   console.log("In Middleware 2");
+  next();
   //   res.send("<h2>Response from Middleware 2</h2>");
-  res.send("Response from Middleware 2");
+});
+
+app.use("/user", (req, res, next) => {
+  console.log("Handled user endpoint");
+  res.send("<h2>Response from user endpoint</h2>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Handled root endpoint");
+  res.send("<h2>Response from root endpoint</h2>");
 });
 
 // const server = http.createServer(app);
