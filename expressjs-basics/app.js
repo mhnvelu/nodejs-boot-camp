98b265687eff622755acdc437461680b2e1f3f85
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-07-08 17:10:13 +0100

    Added body-parser

--- expressjs-basics/app.js
+++ expressjs-basics/app.js
@@ -1,7 +1,9 @@
 // const http = require("http");
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   console.log("-----------------------");
@@ -17,12 +19,27 @@ app.use((req, res, next) => {
 });
 
 app.use("/user", (req, res, next) => {
-  console.log("Handled user endpoint");
+  console.log("Handled /user endpoint");
   res.send("<h2>Response from user endpoint</h2>");
+});
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="product"><button type="submit">Submit</button> </form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log("Handled /product endpoint");
+  const product = req.body;
+  console.log(product);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
   console.log("Handled root endpoint");
+  const product = req.body;
+  console.log(product);
   res.send("<h2>Response from root endpoint</h2>");
 });
 
