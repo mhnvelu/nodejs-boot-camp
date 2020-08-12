3f99f44ad19fb16291ea38f17d0d77506bd17fd6
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-08-12 20:02:00 +0100

    Added Auth middleware to all Routes

--- social-network-app/expressjs-rest-api/routes/feed.js
+++ social-network-app/expressjs-rest-api/routes/feed.js
@@ -9,6 +9,7 @@ router.get("/posts", isAuth, feedController.getPosts);
 
 router.post(
   "/post",
+  isAuth,
   body("title", "Invalid Title").trim().isLength({ min: 5 }).isString(),
   body("content", "Invalid Content")
     .trim()
@@ -21,6 +22,7 @@ router.get("/post/:postId", isAuth, feedController.getPost);
 
 router.put(
   "/post/:postId",
+  isAuth,
   body("title", "Invalid Title").trim().isLength({ min: 5 }).isString(),
   body("content", "Invalid Content")
     .trim()
@@ -29,6 +31,6 @@ router.put(
   feedController.updatePost
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
