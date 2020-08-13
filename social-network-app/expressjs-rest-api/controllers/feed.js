7b2b7a53409eec4abb50ce4d360c67c2d1ad4f46
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-08-13 21:08:51 +0100

    websocket - emit updated post

--- social-network-app/expressjs-rest-api/controllers/feed.js
+++ social-network-app/expressjs-rest-api/controllers/feed.js
@@ -126,14 +126,14 @@ exports.updatePost = async (req, res, next) => {
   }
 
   try {
-    const post = await Post.findById(postId);
+    const post = await Post.findById(postId).populate("creator", "name");
     if (!post) {
       const error = new Error("Post not found");
       error.statusCode = 404;
       throw error;
     }
 
-    if (post.creator.toString() !== req.userId) {
+    if (post.creator._id.toString() !== req.userId) {
       const error = new Error("Not Authorized");
       error.statusCode = 403;
       throw error;
@@ -146,6 +146,11 @@ exports.updatePost = async (req, res, next) => {
     post.content = content;
     post.imageUrl = imageUrl;
     const updatedPost = await post.save();
+
+    io.getIO().emit("posts", {
+      action: "update",
+      post: updatedPost,
+    });
 
     res.status(200).json({
       message: "Post updated successfully",
