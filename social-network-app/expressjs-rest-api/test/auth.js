55d3bb07d414b32b076d700cf7f25673f7de3c40
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-08-19 11:43:32 +0100

    Unit Testing using Mocha, Chai

---
+++ social-network-app/expressjs-rest-api/test/auth.js
@@ -0,0 +1,23 @@
+const authMiddleware = require("../middleware/is-auth");
+const expect = require("chai").expect;
+it("should throw an error if authorization header is not present", () => {
+  const req = {
+    get: (headerName) => {
+      return null;
+    },
+  };
+
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+    "Authentication Failed"
+  );
+});
+
+it("should throw an error if authorization header is only one string", () => {
+  const req = {
+    get: (headerName) => {
+      return "xyz";
+    },
+  };
+
+  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+});
