fc320619f4be2a8c8ecb212229660623bf776802
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-08-19 12:54:53 +0100

    Testing Async code, stub using sinon

--- social-network-app/expressjs-rest-api/controllers/auth.js
+++ social-network-app/expressjs-rest-api/controllers/auth.js
@@ -64,11 +64,13 @@ exports.login = async (req, res, next) => {
     );
 
     res.status(200).json({ token: token, userId: user._id.toString() });
+    return;
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
+    return err;
   }
 };
 exports.getUserStatus = async (req, res, next) => {
--- social-network-app/expressjs-rest-api/test/auth-middleware.js
+++ social-network-app/expressjs-rest-api/test/auth-middleware.js
@@ -1,5 +1,7 @@
 const authMiddleware = require("../middleware/is-auth");
 const expect = require("chai").expect;
+const sinon = require("sinon");
+const jwt = require("jsonwebtoken");
 
 describe("Auth Middleware Unit Test", () => {
   it("should throw an error if authorization header is not present", () => {
@@ -32,5 +34,23 @@ describe("Auth Middleware Unit Test", () => {
     };
 
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should yield userId after decoding the token", () => {
+    const req = {
+      get: (headerName) => {
+        return "Bearer abcxyz";
+      },
+    };
+
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({ userId: "abc" });
+    authMiddleware(req, {}, () => {});
+
+    expect(req).to.have.property("userId");
+    expect(req).to.have.property("userId", "abc");
+    expect(jwt.verify.called).to.be.true;
+
+    jwt.verify.restore();
   });
 });
